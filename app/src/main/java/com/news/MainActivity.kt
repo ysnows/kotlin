2e0778d18e0410bcade5e898cac146d6eda2eade
xianguangjin <1447360366@qq.com>
2016-11-26 16:55:34 +0800

    2016-11-26 16:55:33

---
+++ app/src/main/java/com/news/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.news
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+
+    }
+
+
+    fun changeFragment(f: Fragment, cleanStack: Boolean) {
+        val ft = supportFragmentManager.beginTransaction()
+        if (cleanStack) {
+            clearBackStack()
+        }
+        ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.replace(R.id.content_main, f)
+        ft.addToBackStack(null)
+        ft.commit()
+    }
+
+    fun clearBackStack() {
+        val manager = supportFragmentManager
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0)
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        }
+    }
+
+    override fun onBackPressed() {
+        val manager = supportFragmentManager
+        if (manager.backStackEntryCount > 1) manager.popBackStack() else finish()
+
+    }
+
+}
